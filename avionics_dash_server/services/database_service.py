62f97fcdea6e034de87a28de983bb9ac3f690127
iamibi <8592115+iamibi@users.noreply.github.com>
2022-11-06 10:44:35 -0800

    updated credential based mongo access

--- avionics_dash_server/services/database_service.py
+++ avionics_dash_server/services/database_service.py
@@ -1,13 +1,26 @@
 # Third-Party Library
-from pymongo import MongoClient, database
+from pymongo import MongoClient, database, collection
+
+# Custom Library
+from avionics_dash_server.config.settings import settings
 
 
 class DatabaseService:
     _db: database.Database = None
+    _collections: collection.Collection = None
 
     def __init__(self) -> None:
-        client = MongoClient(host="localhost", port=27017, tz_aware=True)
-        self._db = client["avionics_dash_dev"]
+        db_name = settings.db.avionics_dash.db_name
+        client = MongoClient(
+            host=settings.db.avionics_dash.host,
+            port=settings.db.avionics_dash.port,
+            tz_aware=True,
+            username=settings.credentials.db.username,
+            password=settings.credentials.db.password,
+            authSource=db_name,
+            authMechanism=settings.db.avionics_dash.auth_mechanism,
+        )
+        self._db = client[db_name]
 
     def _create_index(self):
         raise NotImplementedError
--- tests/unit/config/test_settings.py
+++ tests/unit/config/test_settings.py
@@ -1,9 +1,40 @@
+# Standard Library
+from typing import NamedTuple
+
+# Third-Party Library
+import pytest
+
+
 class TestSettings:
-    def test_settings(self):
+    @pytest.fixture(scope="class")
+    def setup(self):
+        # Custom Library
+        from avionics_dash_server.config.settings import settings
+        yield settings
+
+    def test_settings_import(self):
         try:
             # Custom Library
             from avionics_dash_server.config.settings import settings
         except ImportError:
             assert False, "Unable to create settings object"
+
+    def test_settings_value(self, setup):
+        settings = setup
         assert settings is not None
         assert settings.placeholder == "placeholder"
+
+    def test_database_config_value(self, setup):
+        settings = setup
+
+        assert settings.db is not None
+        assert settings.db.avionics_dash is not None
+        assert settings.db.avionics_dash.db_name == "avionics_dash_test"
+
+    def test_credentials_value(self, setup):
+        settings = setup
+
+        assert settings.credentials is not None
+        assert settings.credentials.db is not None
+        assert settings.credentials.db.username == "testUser"
+        assert settings.credentials.db.password == "testPassword"
