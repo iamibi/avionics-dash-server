613955abb412534e2b8cf55aed1746d900132a0d
Tyler Hennig <tyhennig@gmail.com>
2022-11-14 22:24:52 -0500

    move password model to user file

--- avionics_dash_server/models/user_model.py
+++ avionics_dash_server/models/user_model.py
@@ -1,3 +1,9 @@
+# Standard Library
+from datetime import datetime
+
+# Third-Party Library
+from pydantic import BaseModel, validator
+
 from typing import List
 from course_model import Course
 
@@ -23,3 +29,15 @@ class Teacher(User):
 
 class Admin(User):
     role = 'admin'
+
+class PasswordModel(BaseModel):
+    password_hash: str
+    created_at: datetime
+    updated_at: datetime
+
+    @validator("created_at", "updated_at", pre=True, always=True)
+    def check_timestamp(cls, v):
+        # Check the timezone is UTC or not
+        if v.tzname() != "UTC":
+            return ValueError("The timezone is not in UTC format")
+        return v
