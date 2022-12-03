80d53939d5bdd197163ee2e197bb09a11c2746c4
iamibi <8592115+iamibi@users.noreply.github.com>
2022-12-03 12:14:00 -0800

    testing deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app_prod:prod_deploy
+web: gunicorn --bind 0.0.0.0:5000 wsgi:app
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
-from avionics_dash_server.app import app
+# Standard Library
 import logging
+
+# Custom Library
+from avionics_dash_server.app import app
+
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
+    logger.info("Initializing Avionics-Dash!")
     app.run()
