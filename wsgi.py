3ded435d3cb441b35cfbd4d3bebbf886d94d78d3
iamibi <8592115+iamibi@users.noreply.github.com>
2022-12-03 11:37:57 -0800

    testing deployment

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,6 @@
-from avionics_dash_server.app import create_app
+from avionics_dash_server.app import app
 import logging
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    logger.info("Starting Prod Service!")
-
-    # Third-Party Library
-    from dotenv import load_dotenv
-
-    # Load Environment Variables
-    load_dotenv()
-
-    app = create_app()
     app.run()
