bbf52483768ee01db1206a7d93ba9481c6e37dd1
Syed Mohammad Ibrahim <8592115+iamibi@users.noreply.github.com>
2022-12-07 10:55:06 -0800

    add course to user endpoint added (#31)

--- avionics_dash_server/api/api_errors.py
+++ avionics_dash_server/api/api_errors.py
@@ -9,20 +9,20 @@ from avionics_dash_server.common import exceptions as exs
 def api_errors(app: Flask) -> None:
     @app.errorhandler(exceptions.BadRequest)
     def bad_request_error(e):
-        return jsonify({"error": "Bad Request"}), 400
+        return jsonify({"error": "Bad Request", "error_message": str(e)}), 400
 
     @app.errorhandler(exceptions.Unauthorized)
     def unauthorized_error(e):
-        return jsonify({"error": "Unauthorized"}), 401
+        return jsonify({"error": "Unauthorized", "error_message": str(e)}), 401
 
     @app.errorhandler(exs.ValidationError)
     def validation_error(e):
-        return jsonify({"error": "Validation Failed! Bad Request"}), 400
+        return jsonify({"error": "Validation Failed! Bad Request", "error_message": str(e)}), 400
 
     @app.errorhandler(exceptions.Forbidden)
     def forbidden_error(e):
-        return jsonify({"error": "Forbidden"}), 403
+        return jsonify({"error": "Forbidden", "error_message": str(e)}), 403
 
     @app.errorhandler(exs.AuthenticationError)
     def authentication_error(e):
-        return jsonify({"error": "Authentication Failed!"}), 401
+        return jsonify({"error": "Authentication Failed!", "error_message": str(e)}), 401
